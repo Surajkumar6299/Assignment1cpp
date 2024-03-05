218013dd87b4946719e3e139779286b4f696dbe0
Surajkumar6299 <162327311+Surajkumar6299@users.noreply.github.com>
2024-03-05 18:49:46 +0530

    Add files via upload

---
+++ Assignment 1/assignment1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"physics"<<endl;
+    cout<<"wallah"<<endl;
+}
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<5+5<<endl;
+}
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int =4;
+    float=4;
+    bool=1;
+}
+
+
+#include<iostream>
+using namespace std;
+int main() {
+int a = 4;
+int b = 5;
+a++, b--;
+cout << ++a << " " << b--;
+}
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int r=10;
+    float pi=3.14;
+    float circumference=2*pi*r;
+    cout<<circumference;
+
+}
+
+
+#include<iostream>
+using namespace std;
+int main(){
+     01Pwskills=1; // incorrect
+
+     int_FLOAT =2 ;//correct
+    int =4; //incorrect
+    FLOAT  =5;//correct
+    You will succeed //incorrect
+}
+
